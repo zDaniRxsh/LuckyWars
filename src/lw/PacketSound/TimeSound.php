43b742347ff9bee446fbe3ab046bc43d69e96a7f
DragonBoyHD <45833810+DragonBoyHD@users.noreply.github.com>
2019-03-02 21:08:52 -0600

    Create TimeSound.php

---
+++ src/lw/PacketSound/TimeSound.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace lw\PacketSound;
+use pocketmine\level\sound\GenericSound;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+class TimeSound extends GenericSound{
+public function __construct(Vector3 $pos, float $pitch = 0){
+
+		parent::__construct($pos, LevelEventPacket::EVENT_SOUND_CLICK_FAIL, $pitch);
+
+	}
+}
